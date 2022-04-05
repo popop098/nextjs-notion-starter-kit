06d9ceac798c93df2d53df4e7ba6be60f6a8b51f
popop098 <popop0982@naver.com>
2022-04-05 11:03:47 +0900

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,31 +8,7 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
-
-            <link
-              rel='apple-touch-icon'
-              sizes='180x180'
-              href='/apple-touch-icon.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='96x96'
-              href='/favicon-96x96.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='/favicon-32x32.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='16x16'
-              href='/favicon-16x16.png'
-            />
+            <link rel='shortcut icon' href='/logo.png' />
 
             <link rel='manifest' href='/manifest.json' />
           </Head>
